9679c1fe8c5fd7ea8f32ebbb2c5dac4e67f3cc50
Laszlo-VSC <laszloluque@gmail.com>
2022-04-30 14:45:57 +0100

    fixed headers, robots, and sitemap

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var csrf = require('csurf');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+var csrfProtection = csrf({ cookie: true })
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
